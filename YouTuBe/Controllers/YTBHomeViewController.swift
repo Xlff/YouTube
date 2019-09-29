ace9be7be378d12a23c48603537b9a088709b618
Max xie <>
2019-09-29 16:53:59 +0800

    homevc

--- YouTuBe/Controllers/YTBHomeViewController.swift
+++ YouTuBe/Controllers/YTBHomeViewController.swift
@@ -7,23 +7,95 @@
 
 import UIKit
 
-class YTBHomeViewController: UIViewController {
-
+class YTBHomeViewController: UITableViewController {
+    var videos = [Video]()
+    var lastContentOffset: CGFloat = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        tableView.estimatedRowHeight = 300
+        fetchData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func fetchData() {
+        Video.fetchVideos { [weak self] response in
+            guard let strongSelf = self else { return }
+            strongSelf.videos = response
+            strongSelf.videos.shuffle()
+            strongSelf.tableView.reloadData()
+        }
     }
-    */
 
+}
+
+extension YTBHomeViewController: UITableViewDataSource {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! YTBVideoCell
+        cell.set(video: videos[indexPath.row])
+        return cell
+    }
+}
+
+extension YTBHomeViewController: UITableViewDelegate {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NotificationCenter.default.post(YTBConstant.openPlayerViewNotificationName)
+    }
+    
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        NotificationCenter.default.post(name: YTBConstant.hiddenMenuNotificationName, object: self.lastContentOffset > scrollView.contentOffset.y ? false : true)
+    }
+    
+    override func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        self.lastContentOffset = scrollView.contentOffset.y
+    }
+    
+}
+
+
+class YTBVideoCell: UITableViewCell {
+    
+    @IBOutlet weak var videoThumbnailImageView: UIImageView!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var channelPic: UIImageView!
+    @IBOutlet weak var videoTitleLabel: UILabel!
+    @IBOutlet weak var videoDescLabel: UILabel!
+    
+    private func customization() {
+        channelPic.layer.cornerRadius = 24
+        channelPic.clipsToBounds = true
+        durationLabel.layer.borderColor = UIColor.white.cgColor
+        durationLabel.layer.borderWidth = 0.5
+        durationLabel.sizeToFit()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        videoThumbnailImageView.image = UIImage(named: "emptyThumbnail")
+        durationLabel.text = nil
+        channelPic.image = nil
+        videoTitleLabel.text = nil
+        videoDescLabel.text = nil
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customization()
+    }
+}
+
+extension YTBVideoCell {
+    func set(video: Video) {
+        videoThumbnailImageView.image = video.thumbnail
+        durationLabel.text = "\(video.duration.secondsToFormattedString())"
+        durationLabel.layer.borderColor = UIColor.lightGray.cgColor
+        durationLabel.layer.borderWidth = 1.0
+        channelPic.image = video.channel.image
+        videoTitleLabel.text = video.title
+        videoDescLabel.text = "\(video.channel.name)  • \(video.views)"
+    }
 }
--- YouTuBe/Extensions.swift
+++ YouTuBe/Extensions.swift
@@ -72,6 +72,45 @@ extension UIColor {
     
 }
 
+extension Int {
+    func secondsToFormattedString() -> String {
+        let hours = self / 3600
+        let minutes = (self % 3600) / 60
+        let second = (self % 3600) % 60
+        let hoursString: String = {
+            let hs = String(hours)
+            return hs
+        }()
+        
+        let minutesString: String = {
+            var ms = ""
+            if  (minutes <= 9 && minutes >= 0) {
+                ms = "0\(minutes)"
+            } else{
+                ms = String(minutes)
+            }
+            return ms
+        }()
+        
+        let secondsString: String = {
+            var ss = ""
+            if  (second <= 9 && second >= 0) {
+                ss = "0\(second)"
+            } else{
+                ss = String(second)
+            }
+            return ss
+        }()
+        
+        var label = ""
+        if hours == 0 {
+            label =  minutesString + ":" + secondsString
+        } else{
+            label = hoursString + ":" + minutesString + ":" + secondsString
+        }
+        return label
+    }
+}
 
 extension CGFloat {
     public static func random() -> CGFloat {
