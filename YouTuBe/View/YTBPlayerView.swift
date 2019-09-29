ad54e206f08ddb46dca25ef9a77783a34ca40224
Max xie <>
2019-09-29 16:14:54 +0800

    playerview

--- YouTuBe/View/YTBPlayerView.swift
+++ YouTuBe/View/YTBPlayerView.swift
@@ -31,13 +31,27 @@ class YTBPlayerView: UIView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+        customization()
+        Video.fetchVideo { [weak self] downloadedVideo in
+            guard let strongSelf = self else { return }
+            strongSelf.video = downloadedVideo
+            strongSelf.videoPlayer = AVPlayer(url: strongSelf.video.videoLink)
+            let playerLayer = AVPlayerLayer(player: strongSelf.videoPlayer)
+            playerLayer.frame = strongSelf.player.frame
+            playerLayer.videoGravity = .resizeAspectFill
+            
+            strongSelf.player.layer.addSublayer(playerLayer)
+            if strongSelf.state != .hidden {
+                strongSelf.videoPlayer.play()
+            }
+            strongSelf.tableView.reloadData()
+        }
     }
     
     private func customization() {
         self.backgroundColor = .clear
         self.tableView.estimatedRowHeight = 90
-        tableView.tableFooterView = UIView()
+//        tableView.tableFooterView = UIView()
         player.layer.anchorPoint.applying(CGAffineTransform(translationX: -0.5, y: -0.5))
         player.addGestureRecognizer(UITapGestureRecognizer(target: self,
                                                            action: #selector(tapPlayView)))
@@ -81,7 +95,46 @@ class YTBPlayerView: UIView {
         delegate?.didMinimize()
         animate()
     }
-    @IBAction func minimizeGesture(_ sender: Any) {
+    @IBAction func minimizeGesture(_ sender: UIPanGestureRecognizer) {
+        
+        if sender.state == .began {
+            let velocity = sender.velocity(in: nil)
+            self.direction = abs(velocity.x) < abs(velocity.y) ? .up : .left
+        }
+        
+        var finalState = StateOfVC.fullScreen
+        switch self.state {
+        case .fullScreen:
+            let factor = (abs(sender.translation(in: nil).y) / UIScreen.main.bounds.height)
+            changeValues(scaleFactor: factor)
+            delegate?.swipeToMinimize(translation: factor, toState: .minimized)
+            finalState = .minimized
+        case .minimized:
+            if self.direction == .left {
+                finalState = .hidden
+                let factor: CGFloat = sender.translation(in: nil).x
+                delegate?.swipeToMinimize(translation: factor, toState: .hidden)
+            }
+            else {
+                finalState = .fullScreen
+                let factor = 1 - (abs(sender.translation(in: nil).y) / UIScreen.main.bounds.height)
+                changeValues(scaleFactor: factor)
+                delegate?.swipeToMinimize(translation: factor, toState: .fullScreen)
+            }
+            
+        default:
+            break
+        }
+        
+        if sender.state == .ended {
+            self.state = finalState
+            self.animate()
+            delegate?.didEndedSwipe(toState: self.state)
+            if state == .hidden {
+                videoPlayer.pause()
+            }
+        }
+        
     }
     
     @objc func tapPlayView() {
@@ -90,6 +143,45 @@ class YTBPlayerView: UIView {
         delegate?.didmaximize()
         animate()
     }
+}
+
+extension YTBPlayerView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let count = video?.suggestedVideos.count else { return 0 }
+        return count + 1
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = self.video.suggestedVideos[indexPath.row - 1].channelName
+            cell.detailTextLabel?.text = self.video.suggestedVideos[indexPath.row - 1].title
+            cell.imageView?.image = self.video.suggestedVideos[indexPath.row - 1].thumbnail
+            return cell
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "header") as! YTBHeaderCell
+        cell.titleLabel.text = video.title
+        cell.contentLabel.text = "\(video.views) views"
+        cell.likesLabel.text = String(video.likes)
+        cell.disLikesLabel.text = String(video.disLikes)
+        cell.channelTitleLabel.text = video.channel.name
+        cell.channelSubsribers.text = "\(video.channel.subscribers) subscribers"
+        cell.channelImageView.image = video.channel.image
+        cell.channelImageView.layer.cornerRadius = 25
+        cell.channelImageView.clipsToBounds = true
+        cell.selectionStyle = .none
+        return cell
+    }
+}
+
+
+class YTBHeaderCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var disLikesLabel: UILabel!
+    @IBOutlet weak var channelTitleLabel: UILabel!
+    @IBOutlet weak var channelSubsribers: UILabel!
+    @IBOutlet weak var channelImageView: UIImageView!
     
 }
